651a23eaf097acd7ea1339e96421b090393b303b
mrnafstad <halvornafstad@hotmail.com>
2020-08-23 11:04:59 +0200

    website

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+	<div id="app">
+		<div id="header">
+			<Menu />
+			<router-link to="/">Home</router-link>
+			<SignLogIn />
+		</div>
+		<div id="nav">
+			<router-view/>
+		</div>
+	</div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+import SignLogIn from './components/SignLogIn.vue'
+
+export default {
+	name: 'App',
+	components: {
+		Menu,
+		SignLogIn
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-left: auto;
+	margin-right: auto;
+}
+#header {
+	background-color: black;
+	width: 100%;
+	margin: auto;
+}
+
+body {
+	background-color: lightgrey
+}
+
+#nav {
+	margin-left: auto;
+	margin-right: auto;
+	width: 500px;
+}
+
+#nav a {
+	font-weight: bold;
+	color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+	color: #42b983;
+}
+</style>
